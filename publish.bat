c4ee2c98861dc087177f3f34653ec8afd24cc3e2
NicholasKrause <NicholasKrause@users.noreply.github.com>
2017-05-28 20:09:30 -0500

    content

---
+++ publish.bat
@@ -0,0 +1,6 @@
+git config --global user.email NicholasKrause@users.noreply.github.com
+git config --global user.name "NicholasKrause"
+git status .
+git stage .
+git commit -m content
+git push origin gh-pages
